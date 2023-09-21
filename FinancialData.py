90b468f6ffc4f6045db2062f6b0b20f2048a61b3
kamal chanchal <kchanchal78@gmail.com>
2023-09-21 11:03:02 +0530

    Initial Setup

---
+++ FinancialData.py
@@ -0,0 +1,22 @@
+# Define Data File Header as present in Csv Format """
+# time	into	inth	intl		intc	intvwap	intv	v	intoi	oi
+
+class OHLCV:
+    time ="time"
+    into="into"
+    inth="inth"
+    intl ="intl"
+    intc="intc"
+    intvwap="intvwap"
+    v="v"
+    intoi="intoi"
+    oi="oi"
+
+
+
+
+class DataPath:
+
+    filename =  "NIFTY_20220501"
+    path = ".//Data//"+filename+".csv"
+
---
+++ main.py
@@ -0,0 +1,52 @@
+from operator import truediv
+
+import FinancialData as da
+import pandas as pd
+
+if __name__ == '__main__':
+    print("starting..")
+
+
+# new seperate class to load data
+class ReadData:
+
+    def __init__( self ):
+        self.path = da.DataPath.path
+
+        self.df_Ohlc = pd.read_csv(self.path)
+
+    def verify_ohlc_data( self ):
+
+         # if datafraome is empty
+        count_rows = self.df_Ohlc.shape[0]
+        print("Total rows :", count_rows)
+
+         #data contain """slice postion """
+        self.df_Ohlc[ 'creation_date' ] = pd.to_datetime( self.df_Ohlc[da.OHLCV.time ] )
+        starting_time = self.df_Ohlc[ 'creation_date' ].min()
+        last_time = self.df_Ohlc[ 'creation_date' ].max()
+
+        print("Data : Starting from ->",starting_time)
+        print("Data: Last Data time ->",last_time)
+
+
+
+
+
+
+
+
+#  create class to load OHLC data
+class IndexAnalysis:
+
+
+
+    def __init__(self):
+        self.path = da.DataPath.path
+
+        self.df_Ohlc = pd.read_csv(self.path)
+
+        #check if file is empty or not
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
