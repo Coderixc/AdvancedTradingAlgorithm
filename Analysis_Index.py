937901f4f959ed9d28bbef0fbdb682626506576a
kamal chanchal <kchanchal78@gmail.com>
2023-12-16 17:31:17 +0530

    (#114) Yahhoo Finance Analysis on NIFTY Historical Range IS Completed

--- Analysis_Index.py
+++ Analysis_Index.py
@@ -20,6 +20,15 @@ class Metrics :
         self.Body_2_Body_Range = body_2_body_range
 
 
+class Metrics2 :
+    def __init__( self , year , min_wick , max_wick , min_body , max_body ) :
+        self.Year = year
+        self.Min_Wick = min_wick
+        self.Max_Wick = max_wick
+        self.Min_Body = min_body
+        self.Max_Body = max_body
+
+
 class Analysis_Index :
 
     def __init__( self , TradingSymbol ) :
@@ -35,12 +44,13 @@ class Analysis_Index :
 
         self.get_history_data( TradingSymbol )
         self.evaluate_Metrics( )
+        self.print_All_stats( )
 
     def get_history_data( self , TradingSymbol ) :
         try :
 
             """Call Yahoo Finance Function () to get data """
-            self.df_history_data = yf.download( TradingSymbol , start = "2022-01-01" , end = "2023-10-01"
+            self.df_history_data = yf.download( TradingSymbol , start = "2002-01-01" , end = "2023-10-01"
                                                 ).reset_index( )
             # self.df_history_data = data.DataReader( TradingSymbol , start = "2023-01-01" , end = "2023-10-01" )
 
@@ -72,7 +82,7 @@ class Analysis_Index :
             _get_current_processing_year = bar[ Quote.mDate ].year
             if _get_current_processing_year not in List_processing_year_record :
                 List_processing_year_record.append( _get_current_processing_year )
-                print( "Processing Year is  :" , _get_current_processing_year )
+                # print( "Processing Year is  :" , _get_current_processing_year )
 
             try :
                 wick_2_wick = abs( bar[ Quote.mHigh ]-bar[ Quote.mLow ] )
@@ -81,7 +91,7 @@ class Analysis_Index :
                 """ Creaet object For Metrics """
                 O = Metrics( bar[ Quote.mDate ] , wick_2_wick , body_2_body )
 
-                if wick_2_wick > 300 :
+                if wick_2_wick > 900 :
                     print( bar[ Quote.mDate ] , wick_2_wick , body_2_body )
 
                 self.list_metrics.append( O )
@@ -101,22 +111,22 @@ class Analysis_Index :
         max_wick_range = 0
         max_body_range = 0
 
-        processing_year = ""
+        processing_year = 0
 
         metrics = [ ]
 
-        for metric in self.list_metrics :
+        self.List_metrics_return = [ ]
 
+        last_record = len( self.list_metrics )
+        scanning_rows = 0
+        for metric in self.list_metrics :
+            scanning_rows = scanning_rows+1
             if (processing_year != metric.Date.year) :
 
-                if (len( metrics ) != 0) :
-                    """ print Analysis """
-                    print( "Processing year : " , processing_year )
-                    print( "MIN WICK Range :" , min_wick_range )
-                    print( "MIN BODY Range :" , min_body_range )
-
-                    print( "MAX WICK Range :" , max_wick_range )
-                    print( "MAX BODY Range :" , max_body_range )
+                """  Save Last(Process) Recod """
+                if processing_year != 0 :
+                    m = Metrics2( processing_year , min_wick_range , max_wick_range , min_body_range , max_body_range )
+                    self.List_metrics_return.append( m )
 
                 processing_year = metric.Date.year
 
@@ -126,7 +136,7 @@ class Analysis_Index :
                 max_wick_range = metric.Wick_2_Wick_Range
                 max_body_range = metric.Body_2_Body_Range
 
-                metrics.append( processing_year )
+            """ Major Calculation """
 
             if min_body_range > metric.Body_2_Body_Range :
                 min_body_range = metric.Body_2_Body_Range
@@ -140,6 +150,19 @@ class Analysis_Index :
             if max_body_range < metric.Body_2_Body_Range :
                 max_body_range = metric.Body_2_Body_Range
 
+            """ Record last Result """
+
+            if processing_year not in metrics :  # n- 1 Year
+                metrics.append( processing_year )
+
+                # print("MA" ,processing_year )
+
+            if scanning_rows == last_record :  # nth Year
+                print( "Last Record is --" )
+
+                m = Metrics2( processing_year , min_wick_range , max_wick_range , min_body_range , max_body_range )
+                self.List_metrics_return.append( m )
+
             # if(metric.Wick_2_Wick_Range)
 
         # """ print Analysis """
@@ -148,3 +171,16 @@ class Analysis_Index :
         #
         # print( "MAX WICK Range :" , max_wick_range )
         # print( "MAX BODY Range :" , max_body_range )
+        print( "Task evaluate_Metrics is Completed : Now Start get ALl Statitics Result " )
+
+    def print_All_stats( self ) :
+        print( "Processing year ," , "MIN WICK_2_WICK ," , "MAX WICK_2_WICK," ,
+               "MIN BODY_2_BODY," , "MAX BODY_2_BODY,"
+               )
+        for stat in self.List_metrics_return :
+            # print("Processing year :" ,stat.Year , "MIN WICK_2_WICK :" , round(stat.Min_Wick,2), "MAX WICK_2_WICK" , round(stat.Max_Wick,2) ,
+            #       "MIN BODY_2_BODY : ", round(stat.Min_Body,2) , "MAX BODY_2_BODY :" ,round( stat.Max_Body))
+
+            print( stat.Year , "," , round( stat.Min_Wick , 2 ) , "," , round( stat.Max_Wick , 2 ) , "," ,
+                   round( stat.Min_Body , 2 ) , "," , round( stat.Max_Body )
+                   )
